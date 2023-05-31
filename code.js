6370295e1a19525ae1071a88f79ed33f304cccd5
MartinPerezGiordan <martin.perezgiordan@gmail.com>
2023-05-31 11:20:25 -0300

    Almost finished JS logic and add HTML inputs

--- code.js
+++ code.js
@@ -3,11 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 const htmlBoard = document.querySelector('.gameboard')
+const restartBtn = document.querySelector('.restart-btn')
+const msg = document.querySelector('.end-message')
+let gameOn = true;
+const pOneInput = document.querySelector('#pOneName')
+const pTwoInput = document.querySelector('#pTwoName')
+
+
 let turn = 1
 
 
+const playerFactory = (name, marker, turn)=>{
+    return {name, marker, turn}
+}
+
+let playerOne = playerFactory('Player One', 'X', true);
+let playerTwo = playerFactory('Player Two', 'O', false);
+
 const gameboard = (()=>{
-    let board = ['','','O','','','','','O','O'];
+    let board = ['','','','','','','','',''];
+
+
 
     const createBoard = ()=>{
         for(i=0;i<board.length;i++){
@@ -25,6 +41,20 @@ const gameboard = (()=>{
             cell.innerHTML = board[i];
         }
     }
+
+    restartBtn.addEventListener('click',restart)
+    function restart(){
+        board = ['','','','','','','','',''];
+        renderBoard();
+        gameFlow.addMark();
+        msg.innerHTML=''
+        gameOn=true
+        playerOne.name = pOneInput.value
+        playerTwo.name = pTwoInput.value
+        pOneInput.style.display = 'none';
+        pTwoInput.style.display = 'none';
+
+    }
     
     const changeCellValue = (id, marker)=>{
         let i = Number(id.substring(4));
@@ -32,18 +62,17 @@ const gameboard = (()=>{
         console.log(board)
         renderBoard();
     }  
+
+    const getBoard = ()=>board
     
-    return{createBoard, changeCellValue}
+    return{createBoard, changeCellValue, getBoard}
 })();
 gameboard.createBoard();
 
 
-const playerFactory = (name, marker, turn)=>{
-    return {name, marker, turn}
-}
 
-let playerOne = playerFactory('Player One', 'X', true);
-let playerTwo = playerFactory('Player Two', 'O', false);
+
+
 
 
 
@@ -52,23 +81,71 @@ const gameFlow = (()=>{
     let activePlayer = {}
     const addMark = ()=>{
         const cells = document.querySelectorAll('.cell');
-        cells.forEach(cell => {
-            cell.addEventListener('click',()=>{
-                    if(playerOne.turn){
-                        activePlayer.marker = playerOne.marker
-                    }else{
-                        activePlayer.marker = playerTwo.marker
-                    }
-                    gameboard.changeCellValue(cell.id, activePlayer.marker)
-                    playerOne.turn=!playerOne.turn;
-                    return
-            })
-        });
+            cells.forEach(cell => {
+                cell.addEventListener('click',()=>{
+                        if(playerOne.turn){
+                            activePlayer = playerOne
+
+                        }else{
+                            activePlayer = playerTwo
+                        }
+                        if(gameOn){
+                            gameboard.changeCellValue(cell.id, activePlayer.marker)
+                            checkWin(activePlayer)
+                            playerOne.turn=!playerOne.turn;
+                        }
+                        return
+                }, {once:true})
+            });
+        
     }
+
+    const checkWin = (player)=>{
+        let marker = player.marker
+        let name = player.name
+
+        let board = gameboard.getBoard()
+        console.log(name)
+        
+        if(board[0]===marker && board[1]===marker && board[2]===marker ||
+            board[3]===marker && board[4]===marker && board[5]===marker ||
+            board[6]===marker && board[7]===marker && board[8]===marker ||
+            board[0]===marker && board[3]===marker && board[6]===marker ||
+            board[1]===marker && board[4]===marker && board[7]===marker ||
+            board[2]===marker && board[5]===marker && board[8]===marker ||
+            board[0]===marker && board[4]===marker && board[8]===marker ||
+            board[2]===marker && board[4]===marker && board[6]===marker
+            ){
+                msg.innerHTML = player.name + ' wins!!!'
+                gameOn=false
+                console.log(gameOn)
+                pOneInput.style.display = 'block';
+                pTwoInput.style.display = 'block';
+            }
+
+        else if(board[0]!==''&&
+            board[1]!==''&&
+            board[2]!==''&&
+            board[3]!==''&&
+            board[4]!==''&&
+            board[5]!==''&&
+            board[6]!==''&&
+            board[7]!==''&&
+            board[8]!==''){
+            msg.innerHTML ='It`s a tie'
+            gameOn=false
+            pOneInput.style.display = 'block';
+            pTwoInput.style.display = 'block';
+            
+        }
+        
+
+    }
+
+
+
     return{addMark}
 })()
-
-gameFlow.addMark();
 
 
 
